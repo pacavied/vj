b8a627c226ed3fc0601144fafee0aac31ff58af6
francorest <francorest@gmail.com>
2012-11-07 10:19:30 -0300

    ahora si

---
+++ com.example.musicstrike.MainActivity/src/com/example/musicstrike/loseWindowActivity.java
@@ -0,0 +1,45 @@
+package com.example.musicstrike;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class loseWindowActivity extends Activity{
+
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	  
+	        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+	        Intent intent = getIntent();
+	        setContentView(R.layout.lose_layout);
+	        
+	        
+	        
+	        ImageButton goToMenu = (ImageButton) findViewById(R.id.goToMenuButton);
+	        goToMenu.setOnClickListener(new View.OnClickListener() {
+				
+				public void onClick(View v) {
+								
+					finish();
+					
+				}
+			}); 
+	        
+	        
+	        ImageButton tryAgain = (ImageButton) findViewById(R.id.tryAgainButton);
+	        tryAgain.setOnClickListener(new View.OnClickListener() {
+				
+				public void onClick(View v) {
+								
+					finish();
+					Intent intent = new Intent(loseWindowActivity.this,gameClassicActivity.class);
+					startActivity(intent);
+				}
+			}); 
+	        
+	 }
+}
